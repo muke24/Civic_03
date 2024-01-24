5304881f0ac92de77b1ae7905611890b073f7149
Peter Thompson <pjthompson.au@gmail.com>
2024-01-24 20:33:58 +1100

    Optimisations

--- Assets/Scripts/UI/ButtonPanel.cs
+++ Assets/Scripts/UI/ButtonPanel.cs
@@ -1,103 +1,108 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonPanel : MonoBehaviour
 {
-    [SerializeField]
-    private float buttonSmoothing = 5f;
-    [SerializeField]
-    private float sizeMultiplier = 2f;
+	[SerializeField]
+	private float sizeMultiplier = 2f;
 	[SerializeField]
 	private float sizeTime = 1f;
 	[SerializeField]
-	private float backplier = 1f;
+	private float backplier = 1f; // Backwards multiplier
 	[SerializeField]
-	private float forplier = 1f;
+	private float forplier = 1f; // Forwards multiplier
 
-	private enum CurrentPage { Home = 0, Lighting = 1, Settings = 2}
-    private CurrentPage currentPage = CurrentPage.Home;
-	private int lastPage = 0;
-	Vector2 ogSize = Vector2.zero;
+	private enum CurrentPage { Home, Lighting, Settings }
+	private CurrentPage currentPage = CurrentPage.Home;
 
 	[SerializeField]
-	private Transform buttonHighlight;
-    [SerializeField]
-    private TextMask buttonPositions;
+	private RectTransform buttonHighlight;
+	[SerializeField]
+	private Vector3[] buttonPositions;
 
-    private Vector3 highlightScale;
+	private Coroutine highlightCoroutine;
+	private Vector2 originalHighlightSize;
 
-    void Start()
-    {
-        highlightScale = buttonHighlight.localScale;
+	private static readonly float[] sineTable;
+	private static readonly int sineTableSize = 1000;
 
-	}
-
-
-    public void HomeButton()
-    {
-        lastPage = (int)currentPage;
-		currentPage = CurrentPage.Home;
-        OnButtonPress((int)currentPage);
-    }
-
-    public void LightingButton()
-    {
-		lastPage = (int)currentPage;
-		currentPage = CurrentPage.Lighting;
-		OnButtonPress((int)currentPage);
-
-	}
-
-	public void SettingsButton()
-    {
-		lastPage = (int)currentPage;
-		currentPage = CurrentPage.Settings;
-		OnButtonPress((int)currentPage);
-
-	}
-
-	void OnButtonPress(int button)
-    {
-        StopAllCoroutines();
-
-        StartCoroutine(MoveHighlight(buttonPositions.positions[button]));
-    }
-
-	IEnumerator MoveHighlight(Vector3 position)
+	static ButtonPanel()
 	{
-		RectTransform highlightRect = buttonHighlight.GetComponent<RectTransform>();
-		Vector2 originalSize = highlightRect.sizeDelta;
-		Vector3 startPosition = buttonHighlight.position;
+		sineTable = new float[sineTableSize];
+		for (int i = 0; i < sineTableSize; i++)
+		{
+			sineTable[i] = Mathf.Sin(i * Mathf.PI * 2 / sineTableSize);
+		}
+	}
 
+	void Start()
+	{
+		originalHighlightSize = buttonHighlight.sizeDelta;
+	}
+
+	public void OnHomePress()
+	{
+		OnButtonPress(0);
+	}
+
+	public void OnLightingPress()
+	{
+		OnButtonPress(1);
+	}
+
+	public void OnVehiclePress()
+	{
+		OnButtonPress(2);
+	}
+
+	private void OnButtonPress(int page)
+	{
+		CurrentPage newPage = (CurrentPage)page;
+		if (currentPage != newPage)
+		{
+			currentPage = newPage;
+			MoveHighlightToPage(newPage);
+		}
+	}
+
+	private void MoveHighlightToPage(CurrentPage page)
+	{
+		if (highlightCoroutine != null)
+		{
+			StopCoroutine(highlightCoroutine);
+		}
+		highlightCoroutine = StartCoroutine(MoveHighlight(buttonPositions[(int)page]));
+	}
+
+	private IEnumerator MoveHighlight(Vector3 position)
+	{
+		Vector3 startPosition = buttonHighlight.position;
 		float timeElapsed = 0f;
+
 		while (timeElapsed < sizeTime)
 		{
-			// Move the highlight towards the target position
-			buttonHighlight.position = Vector3.Lerp(startPosition, position, timeElapsed / sizeTime);
-
-			// Determine scale factor based on the phase of the animation
 			float progress = timeElapsed / sizeTime;
-			float scale;
-			if (progress < 0.5f) // First half of the animation
-			{
-				// Scale up using forplier
-				scale = Mathf.Sin(progress * Mathf.PI) * forplier;
-			}
-			else // Second half of the animation
-			{
-				// Scale down using backplier
-				scale = Mathf.Sin(progress * Mathf.PI) * backplier;
-			}
-			highlightRect.sizeDelta = originalSize + new Vector2(scale, 0) * sizeMultiplier;
+			buttonHighlight.position = CustomLerp(startPosition, position, progress);
+
+			float scale = SineTableLookup(progress * sineTableSize) * (progress < 0.5f ? forplier : backplier);
+			buttonHighlight.sizeDelta = originalHighlightSize + new Vector2(scale * sizeMultiplier, 0);
 
 			timeElapsed += Time.deltaTime;
 			yield return null;
 		}
 
-		// Set the highlight to the target position and original size at the end of the animation
 		buttonHighlight.position = position;
-		highlightRect.sizeDelta = originalSize;
+		buttonHighlight.sizeDelta = originalHighlightSize;
 	}
 
+	private static Vector3 CustomLerp(Vector3 a, Vector3 b, float t)
+	{
+		return new Vector3(a.x + (b.x - a.x) * t, a.y + (b.y - a.y) * t, a.z + (b.z - a.z) * t);
+	}
+
+	private static float SineTableLookup(float index)
+	{
+		int i = Mathf.Abs((int)index % sineTableSize);
+		return sineTable[i];
+	}
 }
